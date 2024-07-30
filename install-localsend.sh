3a24929f07b97fe7af1cb8892496b2629f3046bc
The Linux IT Guy <158848334+TheLinuxITGuy@users.noreply.github.com>
2024-07-30 11:52:14 -0600

    Add files via upload

---
+++ install-localsend.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+APP_NAME="localsend"
+
+echo -e "\033[0;32m====================================="
+echo -e "\033[1;32mThe Linux IT Guy - EndeavourOS Scripts"
+echo -e "\033[1;32mInstalling $APP_NAME"
+echo -e "\033[0;32m=====================================\033[0m"
+
+#Check if yay is installed
+if ! command -v yay &>/dev/null
+then
+    echo "yay is not installed. Please install yay first."
+    exit 1
+fi
+
+# Check if the app is already installed
+if ! yay -Q $APP_NAME &> /dev/null
+then
+    echo "$APP_NAME is not installed. Installing now..."
+    # Install the app
+    yay -S --noconfirm $APP_NAME
+else
+    echo "$APP_NAME is already installed. Skipping installation."
+fi
---
+++ remove-localsend.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+APP_NAME="localsend"
+
+echo -e "\033[0;32m====================================="
+echo -e "\033[1;32mThe Linux IT Guy - EndeavourOS Scripts"
+echo -e "\033[1;32mRemoving $APP_NAME"
+echo -e "\033[0;32m=====================================\033[0m"
+
+#Check if yay is installed
+if ! command -v yay &>/dev/null
+then
+    echo "yay is not installed. Please install yay first."
+    exit 1
+fi
+
+# Check if the app is installed
+if yay -Q $APP_NAME &> /dev/null
+then
+    echo "$APP_NAME is installed. Removing now..."
+    # Remove the app
+    yay -R --noconfirm $APP_NAME
+else
+    echo "$APP_NAME is not installed. Skipping removal."
+fi
